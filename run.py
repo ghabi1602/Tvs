47f94ec3a0590a0f6d990f53b8a391a0cfa0cc8b
ghabi1602 <ghabi1602@gmail.com>
2025-05-08 13:41:02 +0100

    first commit

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+from flask_socketio import SocketIO
+
 
 flask_app = create_app()
+socketio = SocketIO(flask_app)
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', port='5001', debug=True)
+    socketio.run(host='0.0.0.0', port='5001', debug=True)
